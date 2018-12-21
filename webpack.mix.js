e4b6e17ae024ccf636e52fb4b5949a67d4260a52
Brian Faust <hello@basecode.sh>
2018-12-21 09:48:29 +0200

    chore: move public into docs (github pages)

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   .copyDirectory('resources/images', 'public/images');
+mix.js('resources/js/app.js', 'docs/js')
+   .sass('resources/sass/app.scss', 'docs/css')
+   .copyDirectory('resources/images', 'docs/images');
